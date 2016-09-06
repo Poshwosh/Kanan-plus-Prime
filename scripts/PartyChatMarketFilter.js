41ddf13692551424928a94827d87fb9d41faf26e
Poshwosh <vizimuley1@gmail.com>
2016-09-06 11:44:09 +0100

    Updated readme & added new scripts from master

--- scripts/PartyChatMarketFilter.js
+++ scripts/PartyChatMarketFilter.js
@@ -33,14 +33,20 @@
 var filter = [
     'b>',
     's>',
+    'b:',
+    's:',
+    'b >',
+    's >',
+    'b.',
+    's.',
+    'b/',
+    's/',
     't>',
     'tf>',
     'lf>',
     't4>',
     'l4>',
-    'trad',
-    'b:',
-    's:'
+    'trad'
 ];
 
 // Make all the filters lowercase to be case insensitive.
