ade98e532518c4d7cd5c180d92e517913655a0cc
Poshwosh <vizimuley1@gmail.com>
2016-09-09 21:07:44 +0100

    Updated gitignore, removed broken scripts

--- scripts/(Broken)SkipCutscenesON.disabled.js
+++
@@ -1,5 +0,0 @@
-// Automatically skip any cutscene. Includes even mainstream quest cutscenes, so use with caution. (Created by Step29, converted from mod_sharker by C0ZIEST, fixed by Poshwosh)
-
-var pattern = scan('?? ?? ?? 75 05 39 ?? ?? ?? ?? ?? ?? E0');
-
-patch(pattern.add(3), 0xEB);
